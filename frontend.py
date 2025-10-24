5ea90382333251ea06eb57a542f79207b23726ce
JiaLiu2000 <jial1@umbc.edu>
2025-10-24 18:30:48 -0400

    Add menu items for students and admins

---
+++ frontend.py
@@ -0,0 +1,16 @@
+# student/guest menu items
+MENU_ITEMS = [
+    {"name": "Home", "url": "/"},
+    {"name": "FAQ", "url": "/faq.html"},
+    {"name": "Search", "url": "/search.html"},
+    {"name": "Chat", "url": "/chat.html"}
+]
+
+# admin menu items
+ADMIN_ITEMS = [
+    {"name": "Admin FAQ", "url": "/admin-faq.html"},
+    {"name": "Add FAQ", "url": "/admin-add.html"},
+    {"name": "Edit FAQ", "url": "/admin-edit.html"},
+    {"name": "Remove FAQ", "url": "/admin-remove.html"},
+    {"name": "Admin Search", "url": "/admin-search.html"}
+]
