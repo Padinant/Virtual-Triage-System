be094b5b844ab8e2226dbf3fd84c5f94a42b2f1c
JiaLiu2000 <jial1@umbc.edu>
2025-10-25 08:40:05 -0400

    Update menu items for student and admin
    Added back menu items lost in reorganization.

--- frontend.py
+++ frontend.py
@@ -1,16 +1,19 @@
 # student/guest menu items
 MENU_ITEMS = [
     {"name": "Home", "url": "/"},
-    {"name": "FAQ", "url": "/faq.html"},
+    {"name": "Browse FAQ", "url": "/faq-page.html"},
     {"name": "Search", "url": "/search.html"},
-    {"name": "Chat", "url": "/chat.html"}
+    {"name": "Ask Chatbot", "url": "/chat.html"},
+    {"name": "Contact Us", "url": "https://www.csee.umbc.edu/contact/"},
+    {"name": "Department Website", "url": "https://www.csee.umbc.edu/"},
+    {"name": "How to Use This Tool", "url": "#"}
 ]
 
 # admin menu items
 ADMIN_ITEMS = [
-    {"name": "Admin FAQ", "url": "/admin-faq.html"},
-    {"name": "Add FAQ", "url": "/admin-add.html"},
-    {"name": "Edit FAQ", "url": "/admin-edit.html"},
-    {"name": "Remove FAQ", "url": "/admin-remove.html"},
-    {"name": "Admin Search", "url": "/admin-search.html"}
+    {"name": "DEBUG Admin FAQ", "url": "/admin-faq.html"},
+    {"name": "DEBUG Admin Search", "url": "/admin-search.html"},
+    {"name": "DEBUG Admin Add", "url": "/admin-add.html"},
+    {"name": "DEBUG Admin Edit", "url": "/admin-edit.html"},
+    {"name": "DEBUG Admin Remove", "url": "/admin-remove.html"}
 ]
