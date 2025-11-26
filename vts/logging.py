2ec76e3f3042c0e1856caaf2dbe821413cd5f059
Michael Babich <mbabich1@umbc.edu>
2025-11-26 16:08:31 -0500

    Fix pylint errors in logging.py

--- vts/logging.py
+++ vts/logging.py
@@ -8,11 +8,11 @@ files was not a priority feature of this class project.
 # prefix with timestamp and then UUID
 def generate_unique_filename():
     "Generates a unique filename for the log."
-    pass
+    return "This is a stub for now."
 
 def write_log_item(path, text):
     "Writes some text to a log file."
-    with open(path, mode='a') as logfile:
+    with open(path, mode='a', encoding='utf8') as logfile:
         logfile.write(text)
         logfile.write('\n')
 
@@ -24,11 +24,11 @@ def write_log_entry(path, timestamp, question, answer):
     and "record separator" characters.
     """
     # group separator and timestamp
-    write_log_item(path, '\n\u01D\n')
-    write_log_item(path, time)
+    write_log_item(path, '\n\u001D\n')
+    write_log_item(path, timestamp)
     # record separator and question
-    write_log_item(path, '\n\u01E\n')
+    write_log_item(path, '\n\u001E\n')
     write_log_item(path, question)
     # record separator and answer
-    write_log_item(path, '\n\u01E\n')
+    write_log_item(path, '\n\u001E\n')
     write_log_item(path, answer)
