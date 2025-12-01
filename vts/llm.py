48ff36bad92fdcc9b8051889ab39ef57184b8fef
Michael Babich <mbabich1@umbc.edu>
2025-12-01 01:24:08 -0500

    Fix pylint issues

--- vts/llm.py
+++ vts/llm.py
@@ -9,10 +9,10 @@ note this file and the functions in it are as of now, incomplete.
 SOME FUNCTOINS AND CUSTOM TYPES IN THIS FILE THAT YOU MIGHT WANT TO USE:
 - ask_agent_openai()         for single prompt questions
 - get_agent_response()       for multi-message chats if you have the chat history
-- Message_Type               the format for passing the chat history in multi-message chats
+- MessageType                the format for passing the chat history in multi-message chats
 """
 
-from dataclasses import dataclass, field
+# from dataclasses import dataclass, field
 from typing import List, Dict, Tuple
 
 from openai import OpenAI
@@ -90,49 +90,43 @@ def say_hello_openai() -> str:
     "an example function that can be called at the start!"
     return ask_agent_openai("Say hello and introduce yourself in one short sentence.")
 
-# if this file is ran, have the model introduce itself
-if __name__ == "__main__":
-    print("Demo 1: Calling on agent to say hello (single prompt)...")
-    print(say_hello_openai())
-    print("Demo 2: Having a multi-message with agent")
-    msgs = [{"role": "system", "content": "Say hello and introduce yourself in one short sentence."}]
-    reply, msgs = chat_with_agent_with_history(msgs)
-    print("Reply:")
-    print(reply)
-    print("Messages")
-    print(msgs)
-    print("Finished running llm.py")
-
-
 #########################################################
 # NEW PORTION - TO HELP WITH PASSING FULL CHAT HISTORY TO THE MODEL
 
-def get_agent_client(config_path) -> OpenAI:
+def get_agent_client() -> OpenAI:
     "this function returns an OpenAI client ready to talk to the DigitalOcean agent."
-    base_url, key = load_agent_secret_config(config_path)
+    base_url, key = load_agent_secret_config()
     return OpenAI(base_url=base_url, api_key=key)
 
 
-"""Note that the the message type defined below should follow this structure: 
-{"role": "user"|"assistant"|"system", "content": "..."}
+# "Note that the the message type defined below should follow this structure:
+# {"role": "user"|"assistant"|"system", "content": "..."}
+#
+# note a system message might be something the chatbot can see, but
+# the user may/may not (such as instruction prompts).
+#
+# For example, the system message might be: 'You are a helpful
+# chatbot. Be polite and brief in your responses.'
 
-note a system message might be something the chatbot can see, but the user may/may not (such as instruction prompts). 
-For example, the system message might be: 'You are a helpful chatbot. Be polite and brief in your responses.'
-"""
+MessageType = Dict[str, str]   # {"role": "user"|"assistant"|"system", "content": "..."}
 
-Message_Type = Dict[str, str]   # {"role": "user"|"assistant"|"system", "content": "..."}
-
-def chat_with_agent_with_history(messages: List[Message_Type], config_path: str) -> Tuple[str, List[Message_Type]]:
+def chat_with_agent_with_history(messages: List[MessageType]) -> Tuple[str, List[MessageType]]:
     """
-    Call this function if you want to chat to the chatbot, in a way that involves multiple back and forths!
-    This function sends the full chat history (messages) to the agent and returns:
+    Call this function if you want to chat to the chatbot, in a way
+    that involves multiple back and forths!
+
+    This function sends the full chat history (messages) to the agent
+    and returns:
       - assistant_text: the chatbot's reply content
-      - updated_messages: original chat history plus the model's recent message appended at the end
+      - updated_messages: original chat history plus the model's
+        recent message appended at the end
 
     'messages' must be a list of dicts such as:
-        {"role": "system", "content": "..."}  (optional - note we will probably not need to use the "system" role)
+        {"role": "system", "content": "..."}
         {"role": "user", "content": "..."}
         {"role": "assistant", "content": "..."}
+
+    note that system is optional; will probably not need to use the "system" role
 
     Example call:
         msgs = [
@@ -141,16 +135,19 @@ def chat_with_agent_with_history(messages: List[Message_Type], config_path: str)
         ]
         reply, msgs = chat_with_agent_with_history (msgs)
         print (reply)
+
     """
     # step 0 - set up key vars
     fail_output_message = "Access to agent failed. Maybe take a look at the FAQ section?"
 
     # step 1 - get response from the model
-    client = get_agent_client(config_path)
+    client = get_agent_client()
 
     resp = client.chat.completions.create(
         model="n/a",
         messages=messages,
+        #
+        # pylint:disable-next=fixme
         # todo - implement this part later after fully adding knowledge base
         # extra_body={
         #     "include_retrieval_info": include_retrieval_info,
@@ -206,3 +203,17 @@ def create_new_kb(kb_name, kb_decription = ""):
 def add_kb_to_agent(agent_id: str, kb_id: str):
     "tbi"
     return False
+
+# if this file is ran, have the model introduce itself
+if __name__ == "__main__":
+    print("Demo 1: Calling on agent to say hello (single prompt)...")
+    print(say_hello_openai())
+    print("Demo 2: Having a multi-message with agent")
+    msgs = [{"role": "system",
+             "content": "Say hello and introduce yourself in one short sentence."}]
+    reply, msgs = chat_with_agent_with_history(msgs)
+    print("Reply:")
+    print(reply)
+    print("Messages")
+    print(msgs)
+    print("Finished running llm.py")
