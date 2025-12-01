8a9506382a0a9ae7dccff5c89e0a3d6d84c4cd96
Michael Babich <mbabich1@umbc.edu>
2025-12-01 16:31:19 -0500

    If the config file doesn't exist, then use SQLite

--- vts/config.py
+++ vts/config.py
@@ -22,9 +22,15 @@ def load_config() -> dict:
 
 def load_postgres_config() -> Optional[dict]:
     "Loads the PostgreSQL part of the config if it exists."
-    cfg = load_config()
+    # No config file means no PostgreSQL
+    try:
+        cfg = load_config()
+    except OSError:
+        return None
+    # No database entry also means no PostgreSQL
     if "database" in cfg:
         db_cfg = cfg["database"]
+        # The database entry needs two required fields
         if "username" in db_cfg and "password" in db_cfg:
             return db_cfg
     return None
